6859acdaea11a2561f99d6e8e2cedb204b49d29f
Anthony <anthony@preeminentproductions.com>
2014-04-15 18:10:10 -0700

    updated application layouts

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,11 @@
 class VisitorsController < ApplicationController
 
   def new
+  	Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome, Dude!'
+    flash.now[:alert] = 'My birthday is soon.'
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
   end
 
 
